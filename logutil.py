b71055376c49bd9fd143e558f5e143ce926e9a35
Bilery Zoo <652645572@qq.com>
2021-05-05 23:58:30 +0900

    Update logutil.py

--- logutil.py
+++ logutil.py
@@ -4,7 +4,7 @@
 
 """
 author    : Bilery Zoo(bilery.zoo@gmail.com)
-create_ts : 2020-03-03
+create_ts : 2020-01-01
 program   : *_* log logging handler *_*
 """
 
@@ -14,13 +14,13 @@ import logging
 import functools
 
 
-class Log(object):
+class LOG(object):
     """
     Log logging definition.
     """
 
     def __init__(self, level=logging.INFO, stream=sys.stdout, filename=None, filemode='a', datefmt="%Y-%m-%d %H:%M:%S",
-                 format="%(asctime)s\t%(levelname)s\t< Module: %(module)s, Function: %(funcName)s >\t%(message)s"):
+                 format="%(asctime)s\t[%(levelname)s]\t< %(funcName)s[%(lineno)d] >\t%(message)s"):
         """
         LOG init.
         :param level: arg pass to standard library logging.basicConfig().
@@ -37,7 +37,7 @@ class Log(object):
         self.datefmt = datefmt
         self.format = format
 
-    def logger(self, name=__name__):
+    def formatted_logger(self, name=__name__):
         """
         Logger object generates.
         :param name: Logger name(parameters pass to standard library logging.getLogger()).
@@ -51,71 +51,80 @@ class Log(object):
             "datefmt": self.datefmt,
             "format": self.format,
         }
-        try:
-            if not self.filename:
-                del args["filename"]
-        except KeyError:
-            pass
+        if not self.filename:
+            del args["filename"]
         logging.basicConfig(**args)
         return logging.getLogger(name)
 
+    def defined_logger(self, name=__name__):
+        handler = logging.FileHandler(self.filename)
+        handler.setFormatter(logging.Formatter(fmt=self.format, datefmt=self.datefmt))
+        logger = logging.getLogger(name)
+        logger.setLevel(self.level)
+        logger.addHandler(handler)
+        return logger
 
-def raise_log(raised_except, msg=''):
-    """
-    Raise exception by hand to escape error exit caused by built-in [raise].
-    :param raised_except: Exception object.
-    :param msg: Exception feedback message.
-    :return: Python's built-in exit code.
-        Output Exception of [raised_except].
-    """
-    try:
-        raise raised_except(msg)
-    except raised_except as E:
-        logging.exception(E)
+    @staticmethod
+    def raise_log(raised_except, msg=''):
+        """
+        Raise exception by hand to escape error exit caused by built-in [raise].
+        :param raised_except: Exception object.
+        :param msg: Exception feedback message.
+        :return: Python's built-in exit code.
+            Output Exception of [raised_except].
+        """
+        try: raise raised_except(msg)
+        except raised_except as E: logging.exception(E)
 
+    @staticmethod
+    def log(logger=None, exc_msg='', exit_msg='', check_msg='', check_except=None,
+            is_begin_info=True, is_end_info=True, is_exit=False, is_result_check=False):
+        """
+        Log logging decorator function.
+        :param logger: Logger object(see also logging.getLogger()).
+        :param exc_msg: extra message to return when exceptions catch.
+        :param if_exit: Boolean.
+            Whether to exit or not when catching exception.
+        :param exit_msg: extra message to return when exceptions catch and exit.
+        :param is_result_check: Boolean.
+            Whether or not to check Python's False status result of [func]'s return.
+        :param check_except: Exception object to raise when [result_check].
+        :param check_msg: Exception feedback message to return when [result_check].
+        :return: decorated function [func]'s return.
+        """
+        if not logger: logger = LOG().formatted_logger()
 
-def log(logger=None, exc_msg='', if_exit=False, exit_msg='', result_check=False, check_except=None, check_msg=''):
-    """
-    Log logging decorator function.
-    :param logger: Logger object(see also logging.getLogger()).
-    :param exc_msg: extra message to return when exceptions catch.
-    :param if_exit: Boolean.
-        Whether to exit or not when catching exception.
-    :param exit_msg: extra message to return when exceptions catch and exit.
-    :param result_check: Boolean.
-        Whether or not to check Python's False status result of [func]'s return.
-    :param check_except: Exception object to raise when [result_check].
-    :param check_msg: Exception feedback message to return when [result_check].
-    :return: decorated function [func]'s return.
-    """
-    if not logger:
-        logger = Log().logger()
-
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            result = None
-            try:
-                result = func(*args, **kwargs)
-            except BaseException:
-                logger.exception(exc_msg)
-                if if_exit:
-                    sys.exit(exit_msg)
-            finally:
-                if result_check:
-                    if not result:
-                        raise_log(check_except, check_msg)
-                return result
-        return wrapper
-    return decorator
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                result = None
+                try:
+                    if is_begin_info: logger.info("Program enter into function\t< %s >" % func.__name__)
+                    result = func(*args, **kwargs)
+                except BaseException:
+                    logger.exception(exc_msg)
+                    if is_exit: sys.exit(exit_msg)
+                else: return result
+                finally:
+                    if is_end_info: logger.info("Program leave function\t< %s >" % func.__name__)
+                    if is_result_check and not result: LOG.raise_log(check_except, check_msg)
+            return wrapper
+        return decorator
 
 
 if __name__ == "__main__":
-    LOG_Test = Log
-    # LOG().logger().info("INFO:Come...")
-    # LOG().logger().error("ERROR:Come...")
+    # Log = LOG()
+    # Log.logger().info("INFO:info happened...")
+    # Log.logger().error("ERROR:error happened...")
+    # logging.basicConfig(filename="/var/PCR/err")
+    # logging.error("ppp")
+    # log = LOG(filename="/var/PCR/err").logger()
+    # log.error("ppp")
+    # l1 = LOG().formatted_logger()
+    # l2 = LOG(filename="/var/PCR/jbd").formatted_logger()
+    # l1.error("xxx")
 
-    # @log()
-    # def get(a, b):
-    #     return a / b
-    # get(a=1, b=0)
+    @LOG.log()
+    def get(a, b):
+        return a / b
+    get(a=1, b=0)
